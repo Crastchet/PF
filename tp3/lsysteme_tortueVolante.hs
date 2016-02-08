103ccda53e5af2c8b17670a44df00710612f0a96
crastchet <crastchet@gmail.com>
2016-02-08 21:38:25 +0100

    presque en fait, il reste encore les mots suivants

--- tp3/lsysteme_tortueVolante.hs
+++ tp3/lsysteme_tortueVolante.hs
@@ -99,6 +99,28 @@ lsystemeAnime :: LSysteme -> Config -> Float -> Picture
 lsystemeAnime ls c instant = let i = round instant `mod` 6 in interpreteMot c (ls !! i)
 
 ------------------------------- TESTS -------------------------------
+vonKoch1 :: LSysteme
+vonKoch1 = lsysteme "F" regles
+    where regles 'F' = "F-F++F-F"
+          regles  s  = [s]
+
+vonKoch2 :: LSysteme
+vonKoch2 = lsysteme "F++F++F++" regles
+    where regles 'F' = "F-F++F-F"
+          regles  s  = [s]
+
+hilbert :: LSysteme
+hilbert = lsysteme "X" regles
+    where regles 'X' = "+YF-XFX-FY+"
+          regles 'Y' = "-XF+YFY+FX-"
+          regles  s  = [s]
+
+dragon :: LSysteme
+dragon = lsysteme "FX" regles
+    where regles 'X' = "X+YF+"
+          regles 'Y' = "-FX-Y"
+          regles  s  = [s]
+
 brindille :: LSysteme
 brindille = lsysteme "F" regles
     where regles 'F' = "F[-F]F[+F]F"
@@ -108,6 +130,18 @@ broussaille :: LSysteme
 broussaille = lsysteme "F" regles
     where regles 'F' = "FF-[-F+F+F]+[+F-F-F]"
           regles  s  = [s]
+
+vonKoch1Anime :: Float -> Picture
+vonKoch1Anime = lsystemeAnime vonKoch1 (((-400, 0), 0), 800, 1/3, pi/3, "F+-")
+
+vonKoch2Anime :: Float -> Picture
+vonKoch2Anime = lsystemeAnime vonKoch2 (((-400, -250), 0), 800, 1/3, pi/3, "F+-")
+
+hilbertAnime :: Float -> Picture
+hilbertAnime = lsystemeAnime hilbert (((-400, -400), 0), 800, 1/2, pi/2, "F+-")
+
+dragonAnime :: Float -> Picture
+dragonAnime = lsystemeAnime dragon (((0, 0), 0), 50, 1, pi/2, "F+-")
 
 brindilleAnime :: Float -> Picture
 brindilleAnime = lsystemeAnime brindille (((0, -400), pi/2), 800, 1/3, 25*pi/180, "F+-[]")
