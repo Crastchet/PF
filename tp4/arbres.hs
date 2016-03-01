2b8625033b371c0657c64f2c109d2ebc9feaa941
crastchet <crastchet@gmail.com>
2016-03-01 12:01:16 +0100

    taff du mardi matin

--- tp4/arbres.hs
+++ tp4/arbres.hs
@@ -1,3 +1,4 @@
+--1
 data Arbre coul val =
 	Feuille
 	| Noeud coul val (Arbre coul val) (Arbre coul val)
@@ -5,10 +6,40 @@ data Arbre coul val =
 
 data Couleur = Rouge | Noir
 
---1
+
+--2
 map' :: (coul -> a -> b) -> Arbre coul a -> Arbre coul b
-map' f Feuille = Feuille
+map' _ Feuille = Feuille
 map' f (Noeud c v g d) = Noeud c (f c v) (map' f g) (map' f d)
 
 fold :: (coul -> a -> b -> b -> b) -> b -> (Arbre coul a) -> b
-fold f v 
+fold _ x0 Feuille = x0
+fold f x0 (Noeud c v g d) = f c v (fold f x0 g) (fold f x0 d)
+
+
+--3
+hauteur :: Arbre coul val -> Int -- Faut il prendre en compte le premier noeud et les feuilles ?
+hauteur Feuille = 0
+hauteur (Noeud c v g d) = (max (hauteur g) (hauteur d)) + 1
+
+taille :: Arbre coul val -> Int -- Faut il prendre en compte le premier noeud (pour l'instant la fonction le fait)
+taille Feuille = 0
+taille (Noeud c v g d) = taille g + taille d + 1
+
+hauteur' :: Arbre coul val -> Int
+hauteur' arb = fold (\_ g d -> (max g d) + 1) 0 arb
+
+taille' :: Arbre coul val -> Int
+taille' arb = fold (\_ g d -> g + d + 1) 0 arb
+
+mytree = Noeud "b" 1 
+			(Noeud "b" 2 
+				(Noeud "b" 3 Feuille Feuille)
+				Feuille)
+			(Noeud "b" 2
+				Feuille
+				(Noeud "b" 3
+					(Noeud "b" 4
+						Feuille
+						Feuille)
+					Feuille)) 
